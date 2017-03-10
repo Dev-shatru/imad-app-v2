76f6d8deb3a6068c4f2a65fe146d6df9b785a58a
Dev-shatru <shatrughankrroy@gmail.com>
2017-03-10 18:33:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+}); 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send("Article-two requested and it will served here!");
