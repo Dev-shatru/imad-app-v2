67402596dbb50647f29487f2b2936b4741710a34
Dev-shatru <shatrughankrroy@gmail.com>
2017-03-15 18:19:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = require('crypto');
 var app = express();
 app.use(morgan('combined'));
 
@@ -10,6 +10,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
 
+function hash(input){
+    //how do we create hash
+    var hashed = crypto.pbkdf2Sync();
+}
 app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input);
     res.send(hashedString);
